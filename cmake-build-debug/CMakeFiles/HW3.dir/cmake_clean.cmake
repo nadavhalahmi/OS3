62a92ddfce94fc0b8734b95ddc54cdc2dd8b0a90
nadav <nadavhalahmi@campus.technion.ac.il>
2018-05-30 22:22:02 +0300

    initial

---
+++ cmake-build-debug/CMakeFiles/HW3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW3.dir/main.cpp.obj"
+  "HW3.pdb"
+  "HW3.exe"
+  "HW3.exe.manifest"
+  "libHW3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
